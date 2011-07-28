8ae13ff860bc2ab009c2ed5d2f781213cda5a1f3
Dmitry Gruzd <donotsendhere@gmail.com>
2011-07-28 12:15:21 +0400

    added quakelive.com

---
+++ FirstInstall/quakelive.com.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd /tmp
+#BEFORE THAT ACCEPT LICENSE AGREEMENT AT QUAKELIVE.COM
+wget -N http://cdn.quakelive.com/assets/2011071901/QuakeLivePlugin_433.xpi
+unzip QuakeLivePlugin_433.xpi
+mkdir -pv ~/.mozilla/plugins
+cp plugins/npquakelive.*.so ~/.mozilla/plugins/
+rm -r install.rdf QuakeLivePlugin_433.xpi plugins/

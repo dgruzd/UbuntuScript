91c2e35ea59736461b450ebca64bcc67c0e16b97
Dmitry Gruzd <donotsendhere@gmail.com>
2011-07-28 17:05:51 +0400

    moc working

--- FirstInstall/moc.sh
+++ FirstInstall/moc.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 sudo aptitude install moc 
+mkdir -pv $HOME/.moc
+cp -v /usr/share/doc/moc/examples/config.example.gz $HOME/.moc/
+cd $HOME/.moc
+sudo chown $USER config.example.gz
+gunzip config.example.gz
+mv config.example config
+sed -i 's/WINDOWS-1250/WINDOWS-1251/g' $HOME/.moc/config
+sed -i 's/#ID3v1TagsEncoding/ID3v1TagsEncoding/g' $HOME/.moc/config
+sed -i 's/# XTermTheme/XTermTheme/g' $HOME/.moc/config
 

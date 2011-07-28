d6c730ab8a32691f17ff29d1f1e0b0e92227194c
nyaa <donotsendhere@gmail.com>
2011-07-28 14:03:12 +0400

    irssi

--- FirstInstall/quakelive.com.sh
+++ FirstInstall/quakelive.com.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd /tmp
 #BEFORE THAT ACCEPT LICENSE AGREEMENT AT QUAKELIVE.COM
+#ONLY FIREFOX
 wget -N http://cdn.quakelive.com/assets/2011071901/QuakeLivePlugin_433.xpi
 unzip QuakeLivePlugin_433.xpi
 mkdir -pv ~/.mozilla/plugins

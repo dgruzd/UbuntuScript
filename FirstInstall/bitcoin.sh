9a4885aa3ecb563be46ab8cffaa347629bc0e071
Dmitry Gruzd <donotsendhere@gmail.com>
2011-07-30 22:45:55 +0400

    bitcoin & chromium

---
+++ FirstInstall/bitcoin.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo apt-add-repository ppa:stretch/bitcoin
+sudo apt-get update
+sudo apt-get install bitcoin
---
+++ FirstInstall/chromium.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#Codecs need for this great extension https://chrome.google.com/webstore/detail/bbncpldmanoknoahidbgmkgobgmhnafh
+sudo aptitude install chromium-browser chromium-codecs-ffmpeg-nonfree

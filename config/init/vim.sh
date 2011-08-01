e28665f52e8baad419234f7791e253d1fcef59ca
Dmitry Gruzd <donotsendhere@gmail.com>
2011-08-02 00:23:44 +0400

    vim config upd

--- config/init/vim.sh
+++ config/init/vim.sh
@@ -2,11 +2,11 @@
 config_folder="$HOME/Dropbox/UbuntuScript/config"
 
 mv -i $HOME/.gvimrc $HOME/.gvimrc.backup
-ln -s $config_folder/gvimrc $HOME/.gvimrc
+ln -s $config_folder/vim/gvimrc $HOME/.gvimrc
 
 
 
 mv -i $HOME/.vimrc $HOME/.vimrc.backup
-ln -s $config_folder/vimrc $HOME/.vimrc
+ln -s $config_folder/vim/vimrc $HOME/.vimrc
 ln -s $config_folder/vim/vimfolder $HOME/.vim
 

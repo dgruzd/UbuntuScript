b0a9639ae9676d44eda3f94c5f8ba2d60aae2596
Dmitry Gruzd <donotsendhere@gmail.com>
2011-08-03 23:58:07 +0400

    TTM

--- time_to_move.sh
+++ time_to_move.sh
@@ -1,33 +1,36 @@
 #!/bin/sh
-mv -iv $HOME/TIME_TO_MOVE/ $HOME/TIME_TO_MOVE-old
-mkdir -pv $HOME/TIME_TO_MOVE
+backup_dir="/home/nyaa/TIME_TO_MOVE_`date \+\%Y_\%m_\%d`"
+
+mv -iv $backup_dir $backup_dir-old
+mkdir -pv $backup_dir
+echo $backup_dir
 
 echo "################### Chromium browser	###################"
-cp -r $HOME/.config/chromium/ $HOME/TIME_TO_MOVE
+cp -r $HOME/.config/chromium/ $backup_dir
 
 echo "################### Firefox browser	###################"
-cp -r $HOME/.mozilla/firefox/ $HOME/TIME_TO_MOVE
+cp -r $HOME/.mozilla/firefox/ $backup_dir
 
 echo "################### Bitcoin		###################"
-cp -r $HOME/.bitcoin/ $HOME/TIME_TO_MOVE
+cp -r $HOME/.bitcoin/ $backup_dir
 
 echo "################### Pidgin		###################"
-cp -r $HOME/.purple/ $HOME/TIME_TO_MOVE
+cp -r $HOME/.purple/ $backup_dir
 
 echo "################### Cairo Dock		###################"
-cp -r $HOME/.config/cairo-dock/ $HOME/TIME_TO_MOVE
+cp -r $HOME/.config/cairo-dock/ $backup_dir
 
 echo "################### SSH			###################"
-cp -r $HOME/.ssh/ $HOME/TIME_TO_MOVE
+cp -r $HOME/.ssh/ $backup_dir
 
 echo "################### Dropbox		###################"
-cp -r $HOME/Dropbox/ $HOME/TIME_TO_MOVE
+cp -r $HOME/Dropbox/ $backup_dir
 
 echo "################### Desktop		###################"
-cp -r $HOME/Desktop/ $HOME/TIME_TO_MOVE
+cp -r $HOME/Desktop/ $backup_dir
 
 echo "################### Virtualbox		###################"
-cp -r $HOME/VirtualBox\ VMs/ $HOME/TIME_TO_MOVE
-cp -r $HOME/.VirtualBox/ $HOME/TIME_TO_MOVE
+cp -r $HOME/VirtualBox\ VMs/ $backup_dir
+cp -r $HOME/.VirtualBox/ $backup_dir
 
 

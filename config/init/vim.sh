74f2851bc096c5626ee092daa299699a825d794d
nyaa <donotsendhere@gmail.com>
2011-08-04 16:28:17 +0400

    vim updated

--- config/init/vim.sh
+++ config/init/vim.sh
@@ -7,7 +7,7 @@ ln -v -s $config_folder/gvimrc $HOME/.gvimrc
 
 
 mv -i $HOME/.vimrc $HOME/.vimrc.backup
-ln -v -s $config_folder/vimrc $HOME/.vimrc
+ln -v -s $config_folder/gvimrc $HOME/.vimrc
 mv -i $HOME/.vim $HOME/.vim.backup
 ln -v -s $config_folder/vimfolder $HOME/.vim
 

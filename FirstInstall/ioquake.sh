ef98e8cf73854ad12e6d5165387b0f596dfe2b9a
Dmitry Gruzd <donotsendhere@gmail.com>
2011-08-07 15:19:02 +0400

    ioquake update

--- FirstInstall/ioquake.sh
+++ FirstInstall/ioquake.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
+echo "Type your machine type \"64\" or \"32\" bit (2 digits), followed by [ENTER]:"
+read mtype
+
 
 mkdir -pv $HOME/.q3a/baseq3
 mkdir -pv $HOME/.q3a/missionpack
 cd /tmp
 #http://ioquake3.org/extras/patch-data/
-wget -N http://dl.dropbox.com/u/9175582/quake3-latest-pk3s.zip
+wget -N http://dl.dropbox.com/u/9513848/quake3-latest-pk3s.zip
+wget -N http://dl.dropbox.com/u/9513848/PAK0.PK3
+mv -v PAK0.PK3 $HOME/.q3a/baseq3
 unzip quake3-latest-pk3s.zip
 rm -v quake3-latest-pk3s.zip
 cd quake3-latest-pk3s
 cp -v baseq3/* $HOME/.q3a/baseq3 
 cp -v missionpack/* $HOME/.q3a/missionpack
 rm -rf quake3-latest-pk3s/
-echo "Type your machine type \"64\" or \"32\" bit (2 digits), followed by [ENTER]:"
-read mtype
 if [ "$mtype" = "64" ]; then
   wget -N http://ioquake3.org/files/1.36/installer/ioquake3-1.36-7.1.x86_64.run
   LANG=C sudo sh ioquake3-1.36-7.1.x86_64.run

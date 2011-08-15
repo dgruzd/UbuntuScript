321aec021b1f9a34ae4e2de246a1400527948a46
nyaa <donotsendhere@gmail.com>
2011-08-15 19:56:39 +0400

    git update

---
+++ FirstInstall/git.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+echo "Enter your user.name for git"
+read name
+if [ "$name" ];then
+else
+name = "Dmitry Gruzd"
+fi
+echo "Enter your email for git"
+read email
+if [ "$email" ]; then
+else
+email = "donotsendhere@gmail.com"
+fi
+
+
+
+sudo apt-get install --assume-yes python-software-properties
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install --assume-yes git
+
+git config --global user.name "$name"
+git config --global user.email "$email" 
+git config --global core.editor "vim"
+
+echo "Do you want to generate ssh keys for github?[y/n]"
+read keys
+if [ "$keys" = "y" ]; then
+mkdir -pv $HOME/.ssh
+cd $HOME/.ssh
+#BACKUP OLD KEYS
+mkdir key_backup
+cp id_rsa* key_backup
+rm id_rsa*
+#GENERATE KEYS
+ssh-keygen -t rsa -C "$email"
+echo "COPY YOUR PUB KEY INTO GITHUB:"
+cat id_rsa.pub
+echo "##############################"
+else
+echo "OK"
+fi
--- FirstInstall/git/git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo apt-get install --assume-yes python-software-properties
-sudo add-apt-repository ppa:git-core/ppa
-sudo apt-get update
-sudo apt-get install --assume-yes git
-
-git config --global user.name "Dmitry Gruzd"
-git config --global user.email "donotsendhere@gmail.com" 
-git config --global core.editor "vim"

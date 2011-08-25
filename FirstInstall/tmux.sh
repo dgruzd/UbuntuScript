00b9d2f849a821c5c702310426292f6ccb1e57ac
Dmitry Gruzd <donotsendhere@gmail.com>
2011-08-25 18:13:42 +0400

    tmux & bash_aliases_server

---
+++ FirstInstall/tmux.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#http://habrahabr.ru/blogs/linux/126996/
+sudo apt-get --assume-yes install pidgin
---
+++ config/init/bash_aliases_server.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#mv -iv $HOME/.bash_aliases $HOME/.bash_aliases.backup
+ln -vi -s $HOME/Script/config/bash_aliases_server $HOME/.bash_aliases

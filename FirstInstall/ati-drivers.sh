b89e27cacdbce24ac41317015cfdc36ec49bac30
Dmitry Gruzd <donotsendhere@gmail.com>
2011-08-26 18:37:50 +0400

    dropbox & ati

--- FirstInstall/ati-drivers.sh
+++ FirstInstall/ati-drivers.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#http://wiki.cchtml.com/index.php/Ubuntu_Lucid_Installation_Guide
+
 sudo add-apt-repository ppa:ubuntu-x-swat/x-updates
 sudo apt-get update
 sudo apt-get -y install fglrx
---
+++ FirstInstall/drop32box.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+filename="nautilus-dropbox_0.6.8_i386.deb"    
+
+cd /tmp
+echo "Downloading Dropbox: $filename"
+wget -N http://linux.dropbox.com/packages/$filename
+sudo dpkg -i $filename
+rm $filename

a1cccab01084aa5b39a180d7b21aa58386634fba
Dmitry Gruzd <donotsendhere@gmail.com>
2011-09-03 22:17:25 +0400

    bitcoin script update

--- other/Bitcoin/speed.rb
+++ other/Bitcoin/speed.rb
@@ -5,6 +5,12 @@ url = "http://pool.itzod.ru/apiex.php?act=getuserstats&username=nyaa"
 resp = Net::HTTP.get_response(URI.parse(url))
 data = resp.body
 object = JSON.parse(data)
-speed = object["user_speed"]
-print speed
-puts " Mhash"
+speed = object["user_speed"].to_i
+puts "#{speed} Mhash"
+url = "http://deepbit.net/api/4dd35d9681619786e7000007_7561586659"
+resp = Net::HTTP.get_response(URI.parse(url))
+data = resp.body
+object = JSON.parse(data)
+speed2 = object["hashrate"].to_i
+puts "#{speed2} Mhash"
+puts "Result: #{speed + speed2} Mhash"

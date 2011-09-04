6dae689c9f8f7152f63f2a29c632b720f0070de5
Dmitry Gruzd <donotsendhere@gmail.com>
2011-09-04 20:39:53 +0400

    firefox-ext update

--- FirstInstall/firefox-ext/firefox-ext.sh
+++ FirstInstall/firefox-ext/firefox-ext.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
+gnome-default-applications-properties
 filecontent=( `cat "firefox-ext.txt" `)
 
 for t in "${filecontent[@]}"
 do
 gnome-open $t
 done
+
+
+gnome-default-applications-properties

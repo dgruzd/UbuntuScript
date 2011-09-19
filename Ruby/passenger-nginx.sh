d63b86f46040c45f7db4e2a5c1781895965061b3
Dmitry Gruzd <donotsendhere@gmail.com>
2011-09-19 19:14:04 +0400

    nginx upd

--- Ruby/passenger-nginx.sh
+++ Ruby/passenger-nginx.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 #http://www.modrails.com/documentation/Users%20guide%20Nginx.html
 
-#passenger-config --root
-
 #Curl development headers with SSL support
 sudo apt-get --assume-yes install libcurl4-openssl-dev
+
 rvmsudo passenger-install-nginx-module
 
-cd
+cd /tmp
 git clone git://github.com/jnstq/rails-nginx-passenger-ubuntu.git
 sudo cp -v rails-nginx-passenger-ubuntu/nginx/nginx /etc/init.d/nginx
 sudo chown root:root /etc/init.d/nginx
 rm -rf rails-nginx-passenger-ubuntu/
 
-#sudo /usr/sbin/update-rc.d -f nginx defaults
+echo "Do you want to autostart nginx?[y/n]"
+read autostart
+if [ "$autostart" = "y" ]; then
+sudo /usr/sbin/update-rc.d -f nginx defaults
+fi
+
 sudo gedit /opt/nginx/conf/nginx.conf
 
 sudo /etc/init.d/nginx restart

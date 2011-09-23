47652b531d7fd7846661135d8a25a497c63ba005
Dmitry Gruzd <donotsendhere@gmail.com>
2011-09-23 20:34:53 +0400

    not working

--- config/init/gnome-terminal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-gconftool-2 –set /apps/gnome-terminal/profiles/Default/default_size_columns –type integer 135
-
-gconftool-2 –set \
-/apps/gnome-terminal/profiles/Default/default_size_rows \
-–type integer 25

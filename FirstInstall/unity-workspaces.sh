a152bf1935fe48f63c468f63a7ff5e59eaf7f069
Dmitry Gruzd <donotsendhere@gmail.com>
2011-09-23 19:53:05 +0400

    some updt

--- FirstInstall/unity-workspaces.sh
+++ FirstInstall/unity-workspaces.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#deb http://ppa.launchpad.net/geod/ppa-geod/ubuntu YOUR_UBUNTU_VERSION_HERE main 
 sudo add-apt-repository ppa:geod/ppa-geod
 sudo apt-get update
 sudo apt-get install indicator-workspaces

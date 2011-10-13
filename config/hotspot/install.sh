3ad077c2f26faed0744b3c57bc30ea66a69820cb
Dmitry Gruzd <donotsendhere@gmail.com>
2011-10-13 11:55:54 +0400

    hotspot

---
+++ config/hotspot/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# Install wifi hotspot daemon and dns/dhcp server
+sudo apt-get --assume-yes install hostapd dnsmasq
+# Stop installed services
+sudo service hostapd stop
+sudo service dnsmasq stop
+# Disable autostart on boot
+sudo update-rc.d hostapd disable
+sudo update-rc.d dnsmasq disable
+
+
+cp -iv hostapd.conf /etc/hostapd.conf
+
+cp -iv dnsmasq.conf /etc/dnsmasq.conf

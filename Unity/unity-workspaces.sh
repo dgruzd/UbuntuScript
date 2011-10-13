9cc8fbc25c38bf821b83b6b4bdc60c24aa053685
Dmitry Gruzd <donotsendhere@gmail.com>
2011-10-13 23:26:24 +0400

    changes

--- Unity/unity-workspaces.sh
+++ Unity/unity-workspaces.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#echo "deb http://ppa.launchpad.net/geod/ppa-geod/ubuntu YOUR_UBUNTU_VERSION_HERE main" | sudo tee -a /etc/apt/sources.list
-sudo add-apt-repository ppa:geod/ppa-geod
+echo "deb http://ppa.launchpad.net/geod/ppa-geod/ubuntu natty main" | sudo tee -a /etc/apt/sources.list
+#sudo add-apt-repository ppa:geod/ppa-geod
 sudo apt-get update
-sudo apt-get --assume-yes install indicator-workspaces
+sudo apt-get --assume-yes --force-yes install indicator-workspaces
 

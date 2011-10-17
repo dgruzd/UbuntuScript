a21840feb89b309e43c2d547d33d278087e980db
Dmitry Gruzd <donotsendhere@gmail.com>
2011-10-17 14:55:23 +0400

    some

--- other/changelightgdm.sh
+++ other/changelightgdm.sh
@@ -3,6 +3,6 @@ cd /usr/share/backgrounds
 sudo wget -N http://dl.dropbox.com/u/9513848/199340-1920x1200.jpg
 echo "background=/usr/share/backgrounds/199340-1920x1200.jpg" | sudo tee -a /etc/lightdm/unity-greeter.conf
 sudo grep -n "background" /etc/lightdm/unity-greeter.conf | less
-sudo vim /etc/lightdm/unity-greeter.conf
+sudo gedit /etc/lightdm/unity-greeter.conf
 
 

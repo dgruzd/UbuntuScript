32a0faa1d25fbdd5eb09c54e70809548f56de25a
Dmitry Gruzd <donotsendhere@gmail.com>
2011-10-17 19:13:33 +0400

    keepass

--- config/init/keepassx.sh
+++ config/init/keepassx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -pv $HOME/.config/keepassx
-ln -vi -s $HOME/Script/config/keepassx-config.ini $HOME/.config/keepassx/config.ini
+cp -vi $HOME/Script/config/keepassx-config.ini $HOME/.config/keepassx/config.ini

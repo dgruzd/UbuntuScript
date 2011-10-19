88b759f1b02202e71dc0b89f757fdb8ab983c239
Dmitry Gruzd <donotsendhere@gmail.com>
2011-10-19 13:30:30 +0400

    sdfsfd

--- Ruby/rvm/rvm.sh
+++ Ruby/rvm/rvm.sh
@@ -2,7 +2,7 @@
 sudo apt-get install --assume-yes python-software-properties
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt-get update
-sudo apt-get --assume-yes install git curl build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev ruby-dev
+sudo apt-get --assume-yes install git curl build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev ruby-dev wget
 
 cd /tmp
 wget -N https://rvm.beginrescueend.com/install/rvm

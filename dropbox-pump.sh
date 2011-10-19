50a7d6f0ed417a2d09c807590188b84697d561d4
Dmitry Gruzd <donotsendhere@gmail.com>
2011-10-19 23:59:10 +0400

    small update

--- dropbox-pump.sh
+++ dropbox-pump.sh
@@ -2,9 +2,16 @@
 #http://realmfpa.ru/?p=250
 
 
-sudo apt-get install aptitude vim
-sudo vim /etc/apt/sources.list
-sudo apt-get update && sudo apt-get remove network-manager network-manager-gnome && sudo apt-get install xautomation epiphany-browser
+#https://github.com/nyaa/UbuntuScript/raw/master/dropbox-pump.sh
+
+sudo apt-get --assume-yes install aptitude vim
+#sudo vim /etc/apt/sources.list
+
+sudo sed -i 's/# deb/deb/g' /etc/apt/sources.list
+
+sudo apt-get update
+sudo apt-get --assume-yes remove network-manager network-manager-gnome
+sudo apt-get --assume-yes install xautomation epiphany-browser
 
 
 echo "auto eth0" | sudo tee -a  /etc/network/interfaces
@@ -15,3 +22,5 @@ sudo /etc/init.d/networking restart
 wget https://www.dropbox.com/download?dl=packages/nautilus-dropbox_0.6.9_i386.deb && sudo dpkg -i download*
 wget http://dl.dropbox.com/u/18466625/script.tar && tar xvf script.tar && cd dbhacking/
 
+
+#while [ 1 ]; do sh script db.tt/lspEycOe; done

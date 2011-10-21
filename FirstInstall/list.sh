015be296ee2e02bb9456260a9cafb5b38e053c16
Dmitry Gruzd <donotsendhere@gmail.com>
2011-10-21 14:05:40 +0400

    one dir

--- FirstInstall/list.sh
+++ FirstInstall/list.sh
@@ -5,9 +5,11 @@ SUFFIX="sh"
 
 rm -iv list.txt
 
-#for i in "$DIR"/*.$SUFFIX
-#do
-#echo ${i%%.$SUFFIX} |sed 's#^.*/##' >> list.txt
-#done
-find . -iname "*.sh" | sort | sed 's/\.\///g' |sed 's/\.sh//g' >> list.txt
+for i in "$DIR"/*.$SUFFIX
+do
+echo ${i%%.$SUFFIX} |sed 's#^.*/##' >> list.txt
+done
+#find . -iname "*.sh" | sort | sed 's/\.\///g' |sed 's/\.sh//g' >> list.txt
+
+
 cat list.txt

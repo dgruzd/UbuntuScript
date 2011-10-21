685885a683c8e366ac98312114f057b12b0caac4
Dmitry Gruzd <donotsendhere@gmail.com>
2011-10-21 17:50:02 +0400

    ruby other

--- FirstInstall/kernelnew.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo add-apt-repository ppa:kernel-ppa/ppa
-sudo apt-get update
-#apt-cache showpkg linux-headers
-sudo apt-get install linux-headers-2.6.38-10 linux-headers-2.6.38-10-generic linux-image-2.6.38-10-generic --fix-missing

10b27d763c00aec44b6208621a4718485baf9dac
Dmitry Gruzd <donotsendhere@gmail.com>
2011-10-22 15:32:51 +0400

    url changed

--- FirstInstall/ioquake.sh
+++ FirstInstall/ioquake.sh
@@ -27,11 +27,9 @@ echo "vid_restart" | tee -a $q3config
 
 cd /tmp
 #http://ioquake3.org/extras/patch-data/
-#wget -N http://dl.dropbox.com/u/9513848/quake3-latest-pk3s.zip
-axel http://dl.dropbox.com/u/9513848/quake3-latest-pk3s.zip
+axel http://dl.dropbox.com/u/9175582/quake3-latest-pk3s.zip
 #quake gold cd
-#wget -N http://dl.dropbox.com/u/9513848/PAK0.PK3
-axel http://dl.dropbox.com/u/9513848/PAK0.PK3
+axel http://dl.dropbox.com/u/9175582/PAK0.PK3
 mv -v PAK0.PK3 $HOME/.q3a/baseq3
 unzip quake3-latest-pk3s.zip && rm -v quake3-latest-pk3s.zip
 cd quake3-latest-pk3s
--- other/changelightgdm.sh
+++ other/changelightgdm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /usr/share/backgrounds
-sudo wget -N http://dl.dropbox.com/u/9513848/199340-1920x1200.jpg
+sudo wget -N http://dl.dropbox.com/u/9175582/199340-1920x1200.jpg
 echo "background=/usr/share/backgrounds/199340-1920x1200.jpg" | sudo tee -a /etc/lightdm/unity-greeter.conf
 sudo grep -n "background" /etc/lightdm/unity-greeter.conf | less
 sudo gedit /etc/lightdm/unity-greeter.conf

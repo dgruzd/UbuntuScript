9f72dc7bf56f69ab4df6e05d156428afdd0d0011
Dmitry Gruzd <donotsendhere@gmail.com>
2011-10-29 23:43:55 +0400

    xbmc-codecs

---
+++ FirstInstall/xbmc-codecs.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo apt-get --assume-yes install gstreamer0.10-ffmpeg gstreamer0.10-plugins-bad gstreamer0.10-plugins-bad-multiverse gstreamer0.10-plugins-ugly gstreamer0.10-plugins-ugly-multiverse

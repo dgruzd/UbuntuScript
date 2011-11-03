65bb5b8f89c5e645339bcaca613135c1903b1c07
Dmitry Gruzd <donotsendhere@gmail.com>
2011-11-03 12:40:21 +0400

    upd

--- FirstInstall/steam.sh
+++ FirstInstall/steam.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 sudo apt-get --assume-yes install wine
-cd /tmp
+mkdir -pv /tmp/steam
+cd /tmp/steam
 wget -N http://cdn.steampowered.com/download/SteamInstall.msi
-#chmod +x SteamInstall.msi
-wine SteamInstall.msi
+chmod +x SteamInstall.msi
+#wine SteamInstall.msi
+echo "please launch SteamInstall.msi"
+nautilus /tmp/steam
--- other/css/hlds_install.sh
+++ other/css/hlds_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#http://faq.cs2.ru/readarticle.php?article_id=400i
+#http://faq.cs2.ru/readarticle.php?article_id=400
 #http://forum.csmania.ru/viewtopic.php?f=28&t=3794
 mkdir -pv /tmp/hlds
 cd /tmp/hlds
@@ -11,10 +11,18 @@ chmod +x steam
 echo "#!/bin/sh" >> update-css.sh
 echo "./steam -command update -game 'Counter-Strike Source' -dir ." >> update-css.sh
 chmod +x update-css.sh
+
+echo "#!/bin/sh" >> update-tf.sh
+echo "./steam -command update -game tf -dir ." >> update-tf.sh
+chmod +x update-tf.sh
+
 #./steam -command list
 echo "#!/bin/sh" >> css-launch.sh
 echo "screen -A -m -d -S css-server ./css/srcds_run -game cstrike +maxplayers 16 +map de_dust +sv_lan 1 -insecure" >> css-launch.sh
 echo "#version stats exit" >> css-launch.sh
 chmod +x css-launch.sh
+
+echo "#!/bin/sh" >> tf-launch.sh
+echo "./orangebox/srcds_run -game tf +map cp_badlands" >> tf-launch.sh
 
 ./steam

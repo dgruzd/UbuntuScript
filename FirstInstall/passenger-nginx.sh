5e4275ede2f93455e8fc12d905550a29e27da1e2
Dmitry Gruzd <donotsendhere@gmail.com>
2011-11-25 19:09:46 +0400

    forgotten comments

--- FirstInstall/passenger-nginx.sh
+++ FirstInstall/passenger-nginx.sh
@@ -28,9 +28,9 @@ sudo vim /opt/nginx/conf/nginx.conf
 
 sudo /etc/init.d/nginx restart
 
-echo "server {" | sudo tee -a /opt/nginx/conf/nginx.conf
-echo "  listen 80;" | sudo tee -a /opt/nginx/conf/nginx.conf
-echo "  server_name www.mycook.com;" | sudo tee -a /opt/nginx/conf/nginx.conf
-echo "  root /web/some_app/public;" | sudo tee -a /opt/nginx/conf/nginx.conf
-echo "  passenger_enabled on;" | sudo tee -a /opt/nginx/conf/nginx.conf
-echo "}" | sudo tee -a /opt/nginx/conf/nginx.conf
+echo "#server {" | sudo tee -a /opt/nginx/conf/nginx.conf
+echo "#  listen 80;" | sudo tee -a /opt/nginx/conf/nginx.conf
+echo "#  server_name www.mycook.com;" | sudo tee -a /opt/nginx/conf/nginx.conf
+echo "#  root /web/some_app/public;" | sudo tee -a /opt/nginx/conf/nginx.conf
+echo "#  passenger_enabled on;" | sudo tee -a /opt/nginx/conf/nginx.conf
+echo "#}" | sudo tee -a /opt/nginx/conf/nginx.conf

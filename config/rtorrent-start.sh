f6ab07691f0d7809ee5eb6f829c63b6c3fa47d16
Dmitry Gruzd <donotsendhere@gmail.com>
2011-11-27 17:55:15 +0400

    autostart update

--- config/rtorrent-start.sh
+++ config/rtorrent-start.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-screen -A -m -d -S rtorrent rtorrent
+sudo apt-get --assume-yes install screen vim
+echo "screen -A -m -d -S rtorrent rtorrent" | sudo tee -a /etc/rc.local
+sudo vim /etc/rc.local

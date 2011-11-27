96745db4944a1f3008fdefffae16ae1fe9cd6ca9
Dmitry Gruzd <donotsendhere@gmail.com>
2011-11-27 17:57:43 +0400

    fix

--- config/rtorrent-start.sh
+++ config/rtorrent-start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+USER=`whoami`
 sudo apt-get --assume-yes install screen vim
-echo "screen -A -m -d -S rtorrent rtorrent" | sudo tee -a /etc/rc.local
+echo "sudo -H -u $USER screen -A -m -d -S rtorrent rtorrent" | sudo tee -a /etc/rc.local
 sudo vim /etc/rc.local

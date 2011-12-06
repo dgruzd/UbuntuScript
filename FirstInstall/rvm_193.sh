35aa8d46cae50d2112c3bd77d19b727ba31652ea
Dmitry Gruzd <donotsendhere@gmail.com>
2011-12-06 13:50:13 +0400

    refactoring

--- FirstInstall/rvm_193.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rvm get head
-rvm install ruby-1.9.3-p0
-rvm reload
-rvm --default ruby-1.9.3-p0
--- FirstInstall/skype64.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#dist_name=`lsb_release -c | awk -F":" '{ print $2 }'`
-#echo "deb http://download.skype.com/linux/repos/debian/ stable non-free" | sudo tee -a /etc/apt/sources.list
-#sudo sed -i 's/# deb http:\/\/archive.canonical.com\/ubuntu lucid partner/deb http:\/\/archive.canonical.com\/ubuntu lucid partner/g' /etc/apt/sources.list
-#sudo apt-get update && sudo apt-get --assume-yes install skype
-cd /tmp
-wget -N http://download.skype.com/linux/skype-ubuntu_2.2.0.35-1_amd64.deb
-sudo dpkg -i skype-ubuntu*

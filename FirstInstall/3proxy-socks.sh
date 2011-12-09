bd3d7c7ced2848243e00065278b63329b19fa222
Dmitry Gruzd <donotsendhere@gmail.com>
2011-12-09 21:04:54 +0400

    some impruves of 3proxy

--- FirstInstall/3proxy-socks.sh
+++ FirstInstall/3proxy-socks.sh
@@ -6,7 +6,23 @@ wget -N http://3proxy.ru/0.6.1/3proxy-0.6.1.tgz
 tar xvzf 3proxy-0.6.1.tgz
 cd 3proxy-0.6.1
 make -f Makefile.Linux
-sudo cp -v src/socks /usr/bin/3proxy-socks
+
+
+sudo mkdir -pv /opt/3proxy
+sudo cp -v src/socks /opt/3proxy 
+sudo cp -v src/3proxy /opt/3proxy 
+
+echo "daemon" >> config.cfg
+echo "users login:CL:password" >> config.cfg
+echo "log /dev/null" >> config.cfg
+echo "auth strong" >> config.cfg
+echo "socks -p5190" >> config.cfg
+sudo mv config.cfg /opt/3proxy
+
+sudo vim /opt/3proxy/config.cfg
+echo "launching 3proxy"
+cd /opt/3proxy/
+./3proxy config.cfg
 
 cd /tmp 
 rm -rf 3proxy-0.6.1 3proxy-0.6.1.tgz

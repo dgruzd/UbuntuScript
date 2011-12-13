0355f5c0b44b24a53e20e96ccc77e74490a466da
Dmitry Gruzd <donotsendhere@gmail.com>
2011-12-13 16:37:30 +0400

    chrome key

--- FirstInstall/chrome.sh
+++ FirstInstall/chrome.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "deb http://dl.google.com/linux/deb/ stable non-free main" | sudo tee -a /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A040830F7FAC5991
 sudo apt-get update
 sudo apt-get --assume-yes --force-yes install google-chrome-stable
 #filename="google-chrome-stable_current_amd64.deb"

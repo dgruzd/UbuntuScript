53f58b68dfc718143714a909c5dce0104ef06c86
Dmitry Gruzd <donotsendhere@gmail.com>
2011-12-13 15:09:10 +0400

    pyth

--- FirstInstall/rvm.sh
+++ FirstInstall/rvm.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-sudo apt-get install --assume-yes python-software-properties
-
+#sudo apt-get install --assume-yes python-software-properties
 #sudo add-apt-repository ppa:git-core/ppa
 #sudo apt-get update
 sudo apt-get --assume-yes install git curl build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev ruby-dev wget

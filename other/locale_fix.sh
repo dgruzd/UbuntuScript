6f6ea762612d4767220f1823c8b287ef229de996
Dmitry Gruzd <donotsendhere@gmail.com>
2011-12-19 17:58:04 +0400

    o

--- other/locale_fix.sh
+++ other/locale_fix.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 #LOCALES PROBLEM
-sudo locale-gen en_US ru_RU.UTF-8
+sudo locale-gen en_US ru_RU.UTF-8 && sudo dpkg-reconfigure locales
 #sudo export LANG=ru_RU.UTF-8
-sudo dpkg-reconfigure locales

90511da73d07009d934535de5b721d5ed0da9fcb
Dmitry Gruzd <donotsendhere@gmail.com>
2011-12-21 12:46:31 +0400

    some

---
+++ apt-jentelmen.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#apt-nyaa install chrome
+#apt-nyaa install git
+
+#UNITY#################
+apt-nyaa install unity-workspaces
+apt-nyaa install unity-indicator-multiload
+apt-nyaa install unity-indicator-cpufreq
+apt-nyaa install unity-indicator-virtualbox
+#######################
+
+apt-nyaa install aptitude
+apt-nyaa install vim
+apt-nyaa install axel
+apt-nyaa install firefox
+apt-nyaa install rvm
+apt-nyaa install mysql
+apt-nyaa install nload
+apt-nyaa install nmap
+apt-nyaa install openssh-server
+apt-nyaa install pidgin
+apt-nyaa install proxychains
+apt-nyaa install remmina
+apt-nyaa install rtorrent
+apt-nyaa install screen
+apt-nyaa install tmux
+apt-nyaa install transmission
+apt-nyaa install vim
+apt-nyaa install vim-gnome
+apt-nyaa install virtualbox
+apt-nyaa install vlc
+apt-nyaa install mplayer

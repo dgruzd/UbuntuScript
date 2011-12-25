8fd896b6a0bd915d5143b894dc9878abb23dccd6
Dmitry Gruzd <donotsendhere@gmail.com>
2011-12-25 12:19:00 +0400

    postgresql last ver

--- FirstInstall/postgresql.sh
+++ FirstInstall/postgresql.sh
@@ -3,6 +3,9 @@
 #sudo -u postgres psql postgres
 #\password postgres
 #sudo -u postgres createdb mydb
+#sudo apt-get --assume-yes install postgresql
+#sudo apt-get --assume-yes install libpq-dev
 
-sudo apt-get --assume-yes install postgresql
-sudo apt-get --assume-yes install libpq-dev
+sudo add-apt-repository ppa:pitti/postgresql
+sudo apt-get update
+sudo apt-get --assume-yes install postgresql libpq-dev

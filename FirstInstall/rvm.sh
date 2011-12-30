df92f35b0fbc1ceef4c15bb66fe1177fc99e3681
Dmitry Gruzd <donotsendhere@gmail.com>
2011-12-30 17:07:33 +0400

    aliases & ssl for passenger

--- FirstInstall/rvm.sh
+++ FirstInstall/rvm.sh
@@ -2,7 +2,7 @@
 #sudo apt-get install --assume-yes python-software-properties
 #sudo add-apt-repository ppa:git-core/ppa
 #sudo apt-get update
-sudo apt-get --assume-yes install git curl build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev ruby-dev wget subversion
+sudo apt-get --assume-yes install git curl build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev ruby-dev wget subversion libcurl4-openssl-dev
 
 #bash <<(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 cd /tmp

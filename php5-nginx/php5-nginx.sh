1e4ccd58f75bbf2697b368d858d5dccfde963cbd
Dmitry Gruzd <donotsendhere@gmail.com>
2011-12-31 15:00:32 +0400

    assume yes

--- php5-nginx/php5-nginx.sh
+++ php5-nginx/php5-nginx.sh
@@ -3,7 +3,7 @@
 
 BASE_URL="https://raw.github.com/nyaa/UbuntuScript/master/php5-nginx/"
 
-sudo apt-get install php5-cli php5-cgi build-essential
+sudo apt-get --assume-yes install php5-cli php5-cgi build-essential
 echo "######### building spawn-fcgi #############"
 cd /tmp
 wget http://www.lighttpd.net/download/lighttpd-1.4.18.tar.bz2

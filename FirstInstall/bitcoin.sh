e1ba8e146fa36ee312be1735f1d861d40df011c9
Dmitry Gruzd <donotsendhere@gmail.com>
2012-01-06 15:29:22 +0400

    hm

--- FirstInstall/bitcoin.sh
+++ FirstInstall/bitcoin.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#echo "deb http://ppa.launchpad.net/stretch/bitcoin/ubuntu natty main" | sudo tee -a /etc/apt/sources.list
+#echo "deb http://ppa.launchpad.net/bitcoin/bitcoin/ubuntu natty main" | sudo tee -a /etc/apt/sources.list
 sudo apt-add-repository ppa:bitcoin/bitcoin
 sudo apt-get update
-sudo apt-get --assume-yes install bitcoin
+sudo apt-get --assume-yes install bitcoin-qt

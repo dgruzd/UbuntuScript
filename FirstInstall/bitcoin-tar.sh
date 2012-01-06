89233285676826c0065d38df1c01f4eba35fc50f
Dmitry Gruzd <donotsendhere@gmail.com>
2012-01-06 18:57:22 +0400

    fix

--- FirstInstall/bitcoin-tar.sh
+++ FirstInstall/bitcoin-tar.sh
@@ -4,4 +4,4 @@ wget -N http://dl.dropbox.com/u/9175582/bitcoin.tar.gz
 tar xvzf bitcoin.tar.gz
 sudo mv -v bitcoin /etc/
 rm -rf bitcoin/
-ln -sfv /etc/bitcoin/bin/64/bitcoin-qt /usr/bin/bitcoin
+sudo ln -sfv /etc/bitcoin/bin/64/bitcoin-qt /usr/bin/bitcoin

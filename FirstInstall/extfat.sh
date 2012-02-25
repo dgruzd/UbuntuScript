a650d160515781521d5163926a914b8aec996fcd
Dmitry Gruzd <donotsendhere@gmail.com>
2012-02-25 19:07:58 +0400

    fix

--- FirstInstall/extfat.sh
+++ FirstInstall/extfat.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo add-apt-repository ppa:relan/exfat
 sudo apt-get update
-sudo apt-get install exfat-utils fuse-exfat
-echo "USAGE: sudo mount.exfat-fuse /dev/sdh1 /mnt/"
+sudo apt-get --assume-yes install exfat-utils fuse-exfat
+
+#sudo mount.exfat-fuse /dev/sdh1 /mnt/ 
--- other/extfat.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo add-apt-repository ppa:relan/exfat
-sudo apt-get update
-sudo apt-get --assume-yes install exfat-utils fuse-exfat
-
-#sudo mount.exfat-fuse /dev/sdh1 /mnt/ 

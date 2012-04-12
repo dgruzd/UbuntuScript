57a4003d3ece49b40223f185e2917160902c4493
Dmitry Gruzd <donotsendhere@gmail.com>
2012-04-12 21:56:20 +0400

    some

--- other/wakeonlan.rb
+++ other/wakeonlan.rb
@@ -15,8 +15,9 @@ return ask("Enter your password:  ") { |q| q.echo = "*" }
 end
 
 if HOST && USER
-password = get_password
-Net::SSH.start( HOST, USER,:port => '5190', :password => password ) do|ssh|
+#password = get_password
+#Net::SSH.start( HOST, USER,:port => '5190', :password => password ) do|ssh|
+Net::SSH.start( HOST, USER,:port => '5190') do|ssh|
   puts "logged in"
   puts ssh.exec!('uptime')
   puts ssh.exec!('ether-wake 00:1a:4d:5d:d0:33')

0a9118ab2476c54a01632cd0fd5346615aab8e86
Dmitry Gruzd <donotsendhere@gmail.com>
2012-04-12 22:30:46 +0400

    nginx upgrade

--- FirstInstall/nginx.sh
+++ FirstInstall/nginx.sh
@@ -4,4 +4,9 @@ sudo apt-get update
 sudo apt-get --assume-yes install nginx
 
 
-CONF="/etc/nginx/sites-enabled/default" && echo "## Deny access to any host other than (www.)mydomain.com" | sudo tee $CONF && echo "server {" | sudo tee -a $CONF && echo "  server_name  _;  #default" | sudo tee -a $CONF && echo "  return 444;" | sudo tee -a $CONF && echo "}" | sudo tee -a $CONF
+CONF="/etc/nginx/nginx.conf"
+echo "## Deny access to any host other than (www.)mydomain.com" | sudo tee -a $CONF
+echo "#server {" | sudo tee -a $CONF
+echo "#  server_name  _;  #default" | sudo tee -a $CONF
+echo "#  return 444;" | sudo tee -a $CONF
+echo "#}" | sudo tee -a $CONF

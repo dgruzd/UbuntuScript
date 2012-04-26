507728d8371940c5dcc2d209ed349eea5e4fd681
Dmitry Gruzd <donotsendhere@gmail.com>
2012-04-26 17:54:11 +0400

    fix rvm & aliases

--- FirstInstall/rvm.sh
+++ FirstInstall/rvm.sh
@@ -5,7 +5,7 @@
 sudo apt-get --assume-yes install git curl build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev ruby-dev wget subversion libcurl4-openssl-dev
 
 #bash <<(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
-cd /tmp
+#cd /tmp
 #wget -N https://rvm.beginrescueend.com/install/rvm
 #chmod +x rvm
 #mv rvm rvm.sh
@@ -16,19 +16,20 @@ cd /tmp
 #./rvm.sh
 #rm rvm.sh
 
-curl -L get.rvm.io | bash -s stable
+#curl -L get.rvm.io | bash -s stable
+curl https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer | bash -s stable
 
 #seems like rvm auto add this line to .bashrc
-#cp $HOME/.bashrc $HOME/.bashrc.backup
-#echo "[[ -s \"$HOME/.rvm/scripts/rvm\" ]] && source \"$HOME/.rvm/scripts/rvm\"" >> $HOME/.bashrc
+cp $HOME/.bashrc $HOME/.bashrc.backup
+echo "[[ -s \"$HOME/.rvm/scripts/rvm\" ]] && source \"$HOME/.rvm/scripts/rvm\"" >> $HOME/.bashrc
 
 
 
-#echo "Starting .bashrc:"
-#echo "#######################################"
-#cat $HOME/.bashrc | grep '.rvm'
-#echo "#######################################"
-#echo "Check is above a line with rvm"
+echo "Starting .bashrc:"
+echo "#######################################"
+cat $HOME/.bashrc | grep '.rvm'
+echo "#######################################"
+echo "Check is above a line with rvm"
 
 echo "Now please restart shell or open a new one and install ruby"
 #echo "Ctrl + Shift + T > ./rmv2.sh"

3dd867bab9baa5ea865fc3b9d95ab3121e853bbc
Dmitry Gruzd <donotsendhere@gmail.com>
2012-07-17 17:35:32 +0400

    mplayer youtube

---
+++ mplayer-youtube.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Public domain
+# Author: roman [] tsisyk.com
+#
+# Usage: ./me url [youtube-dl parameters]
+#
+
+#quality
+# 22 - 720p
+# 18 - 480p
+
+COOKIE_FILE=/var/tmp/youtube-dl-cookies.txt
+mplayer -fs  -cache 100000 -cache-min 5 -cookies -cookies-file ${COOKIE_FILE} $(youtube-dl --max-quality=22 -g --cookies ${COOKIE_FILE} $*)

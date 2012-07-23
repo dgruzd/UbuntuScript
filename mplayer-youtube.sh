5d974f206c4335dff4020cb7c1d7f56045ccbfc6
Dmitry Gruzd <donotsendhere@gmail.com>
2012-07-23 12:41:39 +0400

    q3maps

--- mplayer-youtube.sh
+++ mplayer-youtube.sh
@@ -10,5 +10,6 @@
 # 22 - 720p
 # 18 - 480p
 
-COOKIE_FILE=/var/tmp/youtube-dl-cookies.txt
+#COOKIE_FILE=/var/tmp/youtube-dl-cookies.txt
+COOKIE_FILE=~/Dropbox/Docs/youtube-dl-cookies.txt
 mplayer -fs  -cache 100000 -cache-min 5 -cookies -cookies-file ${COOKIE_FILE} $(youtube-dl --max-quality=22 -g --cookies ${COOKIE_FILE} $*)

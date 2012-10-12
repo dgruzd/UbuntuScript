5422a9008e9c5dd7469015003f2ead7ff2ce43c0
Dmitry Gruzd <donotsendhere@gmail.com>
2012-10-12 20:18:59 +0400

    fix avconv

--- FirstInstall/avconv.sh
+++ FirstInstall/avconv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo apt-get --assume-yes install libavcodec-extra-53
+sudo apt-get --assume-yes install libav-tools libavcodec-extra-53
 #IPAD
 #avconv -i <pathofvideo>.mkv -vcodec libx264 -acodec libvo_aacenc -r 30000/1001 <outputfilename>.mp4

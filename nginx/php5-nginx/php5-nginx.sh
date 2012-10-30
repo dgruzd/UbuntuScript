5af68e59c24ff715efa06685d70195c27cb84a63
Dmitry Gruzd <donotsendhere@gmail.com>
2012-10-30 12:27:21 +0400

    all

--- nginx/php5-nginx/php5-nginx.sh
+++ nginx/php5-nginx/php5-nginx.sh
@@ -14,11 +14,11 @@ make
 sudo cp src/spawn-fcgi /usr/bin/spawn-fcgi
 echo "######### downloading php-fastcgi #############"
 cd /usr/bin
-sudo wget -N https://raw.github.com/nyaa/UbuntuScript/master/php5-nginx/php-fastcgi
+sudo wget -N $BASE_URL/php-fastcgi
 sudo chmod 755 /usr/bin/php-fastcgi
 echo "######### downloading init-fastcgi #############"
 cd /etc/init.d/
-sudo wget -N https://raw.github.com/nyaa/UbuntuScript/master/php5-nginx/init-fastcgi
+sudo wget -N $BASE_URL/init-fastcgi
 sudo chmod 755 /etc/init.d/init-fastcgi
 echo "######### launch init-fastcgi #############"
 sudo /etc/init.d/init-fastcgi start
@@ -31,7 +31,7 @@ echo "######### restart nginx ###########"
 sudo /etc/init.d/nginx restart
 echo "######### downloading sample php site conf ########"
 cd /tmp
-wget -N https://raw.github.com/nyaa/UbuntuScript/master/php5-nginx/nginx.conf.sample
+wget -N $BASE_URL/nginx.conf.sample
 vim nginx.conf.sample
 #echo "######## append to nginx.conf ##############"
 #cat nginx.conf.sample | sudo tee -a /opt/nginx/conf/nginx.conf

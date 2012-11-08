b8e39e4b7e63f5981d7b65530268a6ab4cf71f01
Dmitry Gruzd <donotsendhere@gmail.com>
2012-11-08 17:57:56 +0400

    pg

--- FirstInstall/postgresql.sh
+++ FirstInstall/postgresql.sh
@@ -9,3 +9,7 @@
 sudo add-apt-repository ppa:pitti/postgresql
 sudo apt-get update
 sudo apt-get --assume-yes install postgresql libpq-dev postgresql-contrib
+
+
+#sudo -u postgres createuser --superuser `whoami`
+#echo "create database `whoami` owner `whoami`" | sudo -u postgres psql

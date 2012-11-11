3c96c582443a880514ff8ec540e99fa9a049fa0e
Dmitry Gruzd <donotsendhere@gmail.com>
2012-11-11 17:57:21 +0400

    some

--- FirstInstall/nvidia-current.sh
+++ FirstInstall/nvidia-current.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo apt-get --assume-yes install linux-source linux-headers-generic
 sudo apt-add-repository ppa:ubuntu-x-swat/x-updates
 sudo apt-get update
 sudo apt-get install nvidia-current

5bc69ac8e5231a570c8573ca2a1f7232efdcc4d3
Dmitry Gruzd <donotsendhere@gmail.com>
2012-11-27 15:31:31 +0400

    ruby falcon

---
+++ FirstInstall/ruby-falcon.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+rvm get head
+#echo "rvm_configure_env=(CFLAGS=-03)" > ~/.rvmrc
+echo 'rvm_configure_env=(CFLAGS="-march=native -O2")' > ~/.rvmrc
+rvm install 1.9.3 -n perf --patch falcon

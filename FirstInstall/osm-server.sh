6cab4f25916a7cd2400617af63cc1117d1fe557b
Dmitry Gruzd <donotsendhere@gmail.com>
2012-12-10 20:29:52 +0400

    osm

--- FirstInstall/osm-server.sh
+++ FirstInstall/osm-server.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:kakrueger/openstreetmap
-sudo apt-get update
+sudo apt-get --assume-yes install python-software-properties && sudo add-apt-repository ppa:kakrueger/openstreetmap && sudo apt-get update
 sudo apt-get install libapache2-mod-tile
 cd /tmp
 wget http://download.geofabrik.de/openstreetmap/europe/russia-european-part.osm.pbf
 osm2pgsql --slim -C 256 –number-processes 1 russia-european-part.osm.pbf
 
-touch /var/lib/mod_tile/planet-import-complete
-sudo /etc/init.d/renderd restart
+touch /var/lib/mod_tile/planet-import-complete && sudo /etc/init.d/renderd restart
---
+++ FirstInstall/osm-server2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo apt-get install subversion git-core tar unzip wget bzip2 build-essential autoconf libtool libxml2-dev libgeos-dev libpq-dev libbz2-dev proj munin-node munin libprotobuf-c0-dev protobuf-c-compiler libfreetype6-dev libpng12-dev libtiff4-dev libicu-dev libboost-all-dev libgdal-dev libcairo-dev libcairomm-1.0-dev apache2 apache2-dev libagg-dev
+
+sudo apt-get install postgresql-9.1-postgis postgresql-contrib postgresql-server-dev-9.1

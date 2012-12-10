e86cf6fcdf6ef2fba95c82a887d9b98a25d94b1f
Dmitry Gruzd <donotsendhere@gmail.com>
2012-12-10 22:10:11 +0400

    fixes

--- FirstInstall/osm-server.sh
+++ FirstInstall/osm-server.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sudo apt-get --assume-yes install python-software-properties && sudo add-apt-repository ppa:kakrueger/openstreetmap && sudo apt-get update
-sudo apt-get install libapache2-mod-tile
+sudo apt-get --assume-yes install postgresql-9.1-postgis postgresql-contrib postgresql-server-dev-9.1
+sudo apt-get --assume-yes install libapache2-mod-tile
 cd /tmp
-wget http://download.geofabrik.de/openstreetmap/europe/russia-european-part.osm.pbf
-osm2pgsql --slim -C 256 –number-processes 1 russia-european-part.osm.pbf
-
-touch /var/lib/mod_tile/planet-import-complete && sudo /etc/init.d/renderd restart
+#wget http://download.geofabrik.de/openstreetmap/europe/russia-european-part.osm.pbf
+#osm2pgsql --slim -C 256 –number-processes 1 russia-european-part.osm.pbf
+#sudo touch /var/lib/mod_tile/planet-import-complete && sudo /etc/init.d/renderd restart

779527836ba9427d98b07d8434143c6cb65b3acb
Dmitry Gruzd <donotsendhere@gmail.com>
2012-12-11 00:06:58 +0400

    locales

--- FirstInstall/osm-server.sh
+++ FirstInstall/osm-server.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-sudo apt-get --assume-yes install python-software-properties && sudo add-apt-repository ppa:kakrueger/openstreetmap && sudo apt-get update
+sudo locale-gen ru_RU.utf8
+sudo dpkg-reconfigure locales
+
+sudo apt-get --assume-yes install python-software-properties
+sudo add-apt-repository ppa:kakrueger/openstreetmap && sudo apt-get update
 sudo apt-get --assume-yes install postgresql-9.1-postgis postgresql-contrib postgresql-server-dev-9.1
 sudo apt-get --assume-yes install libapache2-mod-tile
-cd /tmp
+#cd /tmp
 #wget http://download.geofabrik.de/openstreetmap/europe/russia-european-part.osm.pbf
 #osm2pgsql --slim -C 256 –number-processes 1 russia-european-part.osm.pbf
 #sudo touch /var/lib/mod_tile/planet-import-complete && sudo /etc/init.d/renderd restart

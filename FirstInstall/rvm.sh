e86cdfc0140caedc0e2d0c9036c0a2f936bb198e
Dmitry Gruzd <donotsendhere@gmail.com>
2012-12-14 12:05:53 +0400

    change

--- FirstInstall/rvm.sh
+++ FirstInstall/rvm.sh
@@ -6,7 +6,7 @@
   sudo apt-get --assume-yes install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion
 # Additional packages
 sudo apt-get --assume-yes install libsqlite3-0 ruby-dev wget libcurl4-openssl-dev libsasl2-dev libmemcached-dbg gettext
-sudo apt-get --assume-yes install imagemagick libmagickwand-dev
+sudo apt-get --assume-yes install imagemagick libmagickcore-dev libmagickwand-dev
 #libmemcached-dev libmemcached-dev
 curl -L get.rvm.io | bash -s stable
 #seems like rvm auto add this line to .bashrc

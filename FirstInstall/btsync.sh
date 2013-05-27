00b25fd1b66fbf571152c7aeb6b100c4e2e6ab3c
Dmitry Gruzd <donotsendhere@gmail.com>
2013-05-27 13:20:07 +0400

    btsync

--- FirstInstall/btsync.sh
+++ FirstInstall/btsync.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
+
 cd /tmp
 
-URL=http://btsync.s3-website-us-east-1.amazonaws.com
-wget32="ioquake3-1.36-7.1.i386.run"
-wget64="ioquake3-1.36-7.1.x86_64.run"
+URL=https://github.com/nyaa/UbuntuScript/raw/master/other/btsync
+wget32="btsync_i386.tar.gz"
+wget64="btsync_x64.tar.gz"
+config=$HOME/.btsync.rc
 
-
-MACHINE_TYPE=`uname -m`
-if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+if [ $(uname -m) == 'x86_64' ]; then
+  filename=$wget64
   wget -N $URL/$wget64
 else
+  filename=$wget32
   wget -N $URL/$wget32
 fi
+
+mkdir -pv btsync
+tar xvzf $filename -C btsync
+sudo mv btsync/btsync /usr/bin/
+rm -rf btsync
+
+
+btsync --dump-sample-config > $config
+mkdir -pv $HOME/.sync
+
+sed -i "s/home\/user/home\/$(whoami)/" $config
+sed -i "s/My\sSync\sDevice/$(cat /etc/hostname)/" $config
+
+"${EDITOR:-vi}" $config
+
+
+
+echo "#sudo -u $(whoami) -H btsync --config $config" | sudo tee -a /etc/rc.local
+sudo "${EDITOR:-vi}" /etc/rc.local

1231453b1faa4049c8dcf023f126a8492889325b
Dmitry Gruzd <donotsendhere@gmail.com>
2013-06-18 15:59:51 +0400

    some

---
+++ FirstInstall/glippy.sh
@@ -0,0 +1,3 @@
+sudo add-apt-repository ppa:bikooo/glippy
+sudo apt-get update
+sudo apt-get --assume-yes install glippy
---
+++ FirstInstall/java-7-oracle.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get --assume-yes install oracle-java7-installer
+#sudo update-java-alternatives -s java-7-oracle

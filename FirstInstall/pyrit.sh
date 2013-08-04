7d9fd0eced52f8330d976c1ea826946eac9c2f3a
Dmitry Gruzd <donotsendhere@gmail.com>
2013-08-04 23:46:40 +0400

    pyrit

---
+++ FirstInstall/pyrit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt-get --assume-yes install python-dev libpcap-dev
+
+mkdir -p /tmp/pyrit
+cd /tmp/pyrit
+
+svn checkout http://pyrit.googlecode.com/svn/trunk/ pyrit-read-only
+cd pyrit-read-only
+cd pyrit
+sudo python setup.py install

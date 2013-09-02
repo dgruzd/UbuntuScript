0a5e4789bedbe7ac4207e5406215fdb244695dd9
Dmitry Gruzd <donotsendhere@gmail.com>
2013-09-03 01:28:59 +0400

    mv

---
+++ FirstInstall/btsync-old.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd /tmp
+
+URL=https://github.com/nyaa/UbuntuScript/raw/master/other/btsync
+wget32="btsync_i386.tar.gz"
+wget64="btsync_x64.tar.gz"
+config=$HOME/.btsync.rc
+
+if [ $(uname -m) == 'x86_64' ]; then
+  filename=$wget64
+  wget -N $URL/$wget64
+else
+  filename=$wget32
+  wget -N $URL/$wget32
+fi
+
+mkdir -pv btsync
+tar xvzf $filename -C btsync
+sudo mv btsync/btsync /usr/bin/
+rm -rf btsync
+
+if [ "$1" = "upgrade" ]; then
+  exit
+else
+  btsync --dump-sample-config > $config
+  mkdir -pv $HOME/.sync
+
+  sed -i "s/home\/user/home\/$(whoami)/" $config
+  sed -i "s/My\sSync\sDevice/$(cat /etc/hostname)/" $config
+
+  "${EDITOR:-vi}" $config
+
+  echo "#sudo -u $(whoami) -H btsync --config $config" | sudo tee -a /etc/rc.local
+  sudo "${EDITOR:-vi}" /etc/rc.local
+fi
--- FirstInstall/btsync-ppa.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo add-apt-repository ppa:tuxpoldo/btsync
-sudo apt-get update
-sudo apt-get install btsync

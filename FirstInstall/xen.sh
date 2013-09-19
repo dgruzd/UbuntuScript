08e29d88277e3984b2b3e956e17800cd128e0746
Dmitry Gruzd <donotsendhere@gmail.com>
2013-09-19 16:46:02 +0400

    fix

--- FirstInstall/xen.sh
+++ FirstInstall/xen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install linux-image-server
+sudo apt-get -y install linux-image-server
 sudo apt-get -y install xen-utils-4.1 xen-utils-common
 sudo apt-get -y install xen-hypervisor-4.1-amd64
 sudo apt-get -y install xenstore-utils

c15c511c6292c0a73f7127b529ccd8823b64929d
Dmitry Gruzd <donotsendhere@gmail.com>
2013-11-22 12:59:18 +0400

    plex

---
+++ FirstInstall/plex.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#sudo apt-get -y install avahi-daemon avahi-utils
+cd /tmp
+wget http://downloads.plexapp.com/plex-media-server/0.9.8.10.215-020456b/plexmediaserver_0.9.8.10.215-020456b_amd64.deb
+sudo dpkg -i plexmediaserver_0.9.8.10.215-020456b_amd64.deb
+sudo apt-get -f install

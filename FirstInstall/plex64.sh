d21a92bcea70f996d31a27a2378327d2fd315ff3
Dmitry Gruzd <donotsendhere@gmail.com>
2014-01-09 21:09:12 +0400

    plex

--- FirstInstall/plex64.sh
+++ FirstInstall/plex64.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get -y install avahi-daemon avahi-utils
 cd /tmp
-wget http://downloads.plexapp.com/plex-media-server/0.9.8.10.215-020456b/plexmediaserver_0.9.8.10.215-020456b_amd64.deb
-sudo dpkg -i plexmediaserver_0.9.8.10.215-020456b_amd64.deb
+wget -O plex64.deb http://downloads.plexapp.com/plex-media-server/0.9.8.18.290-11b7fdd/plexmediaserver_0.9.8.18.290-11b7fdd_amd64.deb
+sudo dpkg -i plex64.deb
 #sudo apt-get -f install
--- FirstInstall/postgresql.sh
+++ FirstInstall/postgresql.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
+dist_name=`lsb_release -c | awk -F":" '{ print $2 }'| sed -e 's/^[ \t]*//'`
 
-#sudo -u postgres psql postgres
-#\password postgres
-#sudo -u postgres createdb mydb
-#sudo apt-get --assume-yes install postgresql
-#sudo apt-get --assume-yes install libpq-dev
+sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ $dist_name-pgdg main" >> /etc/apt/sources.list.d/pgdg.list
 
-sudo add-apt-repository ppa:pitti/postgresql
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-#sudo apt-get --assume-yes install postgresql libpq-dev postgresql-contrib
-sudo apt-get -y install postgresql-9.2 postgresql-server-dev-9.2 postgresql-contrib-9.2
+sudo apt-get install libpq-dev postgresql-9.3
 
 
 #sudo -u postgres createuser --superuser `whoami`

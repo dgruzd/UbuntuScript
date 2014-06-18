2904653a732ec34f24a9cdfe29589122820b95e2
Dmitry Gruzd <donotsendhere@gmail.com>
2014-06-18 10:26:35 +0400

    Some again

--- FirstInstall/bluez.sh
+++ FirstInstall/bluez.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#http://blog.asiantuntijakaveri.fi/2012/12/setting-up-xbmcbuntu-12-based-htpc-part_8278.html
+#http://wiki.xbmc.org/index.php?title=How-to:Setup_PS3_BD_Remote
 
 sudo apt-get purge bluez
 sudo add-apt-repository ppa:kitlaan/ppa
---
+++ FirstInstall/nvidia.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo add-apt-repository ppa:xorg-edgers/ppa -y
+sudo apt-get update
+
+echo "run sudo apt-get install nvidia-331"
+echo "or sudo apt-get install nvidia-current"
+echo "or other driver version for your video card"

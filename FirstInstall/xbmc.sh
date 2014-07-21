39ad4991d41b7701e86a6f1dba452721a65a8aae
Dmitry Gruzd <donotsendhere@gmail.com>
2014-07-21 12:25:47 +0400

    Small fixes

--- FirstInstall/xbmc.sh
+++ FirstInstall/xbmc.sh
@@ -3,7 +3,7 @@
 #http://wiki.xbmc.org/index.php?title=HOW-TO_set_up_HDMI_audio_on_nVidia_GeForce_G210,_GT220,_or_GT240
 #plughw:X,7
 # SCREEN artefacts fix is Sync to vblanc in compizconfig options and maybe new drivers YEEES & enable vertical sync in it :)
-sudo apt-get install python-software-properties pkg-config
+sudo apt-get -y install python-software-properties pkg-config
 sudo add-apt-repository ppa:team-xbmc
 #echo "deb http://ppa.launchpad.net/team-xbmc/ppa/ubuntu lucid main" | sudo tee -a /etc/apt/sources.list
 #unstable
---
+++ FirstInstall/youtube-dl.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo wget --no-check-certificate https://yt-dl.org/downloads/latest/youtube-dl -O /usr/bin/youtube-dl
+sudo chmod a+x /usr/bin/youtube-dl

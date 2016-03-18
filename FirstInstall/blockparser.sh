5124cd2a3dcbb977d9206bc0c12766c624bd49c6
Dmitry Gruzd <donotsendhere@gmail.com>
2016-03-18 15:18:25 +0300

    Blockparser

---
+++ FirstInstall/blockparser.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+sudo apt-get install libssl-dev build-essential g++ libboost-all-dev libsparsehash-dev git-core perl
+
+echo "git clone git://github.com/znort987/blockparser.git && cd blockparser && ./make"
